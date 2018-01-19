d1e6bd29084e8aba890e4e884fd5a0a85f9f19db
Matthew Flatt <mflatt@racket-lang.org>
2018-01-19 06:45:24 -0700

    cs: fix `_path` for #f/NULL

--- racket/src/io/path/ffi.rkt
+++ racket/src/io/path/ffi.rkt
@@ -9,6 +9,7 @@
 (define/who _path
   (make-ctype _bytes
               (lambda (p)
-                (check who path-string? p)
-                (->host p #f '()))
-              (lambda (bstr) (path (bytes->immutable-bytes bstr) (system-path-convention-type)))))
+                (check who path-string? #:or-false p)
+                (and p (->host p #f '())))
+              (lambda (bstr) (and bstr (path (bytes->immutable-bytes bstr)
+                                             (system-path-convention-type))))))
