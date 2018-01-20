6b97017ec6fa9997d36e4d5d8de1858a0d504ef5
Matthew Flatt <mflatt@racket-lang.org>
2018-01-19 17:00:43 -0700

    ffi/unsafe: update `(_bytes o ...)` for CS

--- racket/collects/ffi/unsafe.rkt
+++ racket/collects/ffi/unsafe.rkt
@@ -1068,15 +1068,30 @@
 (define-fun-syntax _bytes*
   (syntax-id-rules (o)
     [(_ o n) (type: _gcpointer
-              pre:  (let ([bstr (make-sized-byte-string (malloc (add1 n)) n)])
-                      ;; Ensure a null terminator, so that the result is
-                      ;; compatible with `_bytes`:
-                      (ptr-set! bstr _byte n 0)
-                      bstr)
+              pre:  (make-bytes-argument n)
               ;; post is needed when this is used as a function output type
-              post: (x => (make-sized-byte-string x n)))]
+              post: (x => (receive-bytes-result x n)))]
     [(_ . xs) (_bytes . xs)]
     [_ _bytes]))
+
+(define (make-bytes-argument n)
+  (cond
+    [(eq? 'racket (system-type 'vm))
+     (define bstr (make-sized-byte-string (malloc (add1 n)) n))
+     ;; Ensure a null terminator, so that the result is
+     ;; compatible with `_bytes`:
+     (ptr-set! bstr _byte n 0)
+     bstr]
+    [else (make-bytes n)]))
+
+(define (receive-bytes-result x n)
+  (cond
+    [(eq? 'racket (system-type 'vm))
+     (make-sized-byte-string x n)]
+    [else
+     (define bstr (make-bytes n))
+     (memcpy bstr x n)
+     bstr]))
 
 ;; _bytes/nul-terminated copies and includes a nul terminator in a
 ;; way that will be more consistent across Racket implementations
